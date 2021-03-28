3d2cdf3fa94b66c4059974484e25ce80cc9b05e9
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-28 19:41:38 +0900

    Stateを知る(useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 /**
@@ -8,13 +8,20 @@ import ColorfulMessage from "./components/ColorfulMessage";
  * このような命名方法を「パスカルケース」と呼ぶ
  */
 const App = () => {
-  const onClickButton = () => alert("Clicked");
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
+  // State：それぞれのコンポーネントが持っている可変の状態
+  // stateを使用するには、useState()を用いる
+  // 配列を宣言する　第一引数：stateの変数名、第二引数：stateを更新していく更新名
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">書き方その2</ColorfulMessage>
       <ColorfulMessage color="pink">Not too bad</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
