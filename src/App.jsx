0f0dc784bffe4d837eb879bd9ec828800aca6227
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-28 20:12:18 +0900

    再レンダリングと副作用を知る(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 /**
@@ -8,20 +10,41 @@ import ColorfulMessage from "./components/ColorfulMessage";
  * このような命名方法を「パスカルケース」と呼ぶ
  */
 const App = () => {
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   // State：それぞれのコンポーネントが持っている可変の状態
   // stateを使用するには、useState()を用いる
   // 配列を宣言する　第一引数：stateの変数名、第二引数：stateを更新していく更新名
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">書き方その2</ColorfulMessage>
       <ColorfulMessage color="pink">Not too bad</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^♪</p>}
+      {/* 論理演算子->左辺&&右辺　(左辺がtrueの時に右辺を返す)　faceShowFlagがtrueの時に<p>(^^♪</p>を返す */}
     </>
   );
 };
